47aadaa1efa35d51490fbeeb7668512fa411ab71
esakki29 <esakkilekha@gmail.com>
2020-11-12 12:08:49 +0530

    Update ValidSquare.js

--- ValidSquare.js
+++ ValidSquare.js
@@ -1,4 +1,4 @@
-/* 593. Valid Square
+/* 593. Valid Square - amazon,FB,Google, Netflix
 Given the coordinates of four points in 2D space, return whether the four points could construct a square.
 
 The coordinate (x,y) of a point is represented by an integer array with two integers.
